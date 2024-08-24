c53acd94dbadd0070c780b3de01a4a823465dc2e
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2024-08-24 10:41:00 +0000

    Stuff

--- src/animismic/lib/particles.clj
+++ src/animismic/lib/particles.clj
@@ -13,43 +13,11 @@
 ;;
 ;;
 (defprotocol ParticleField
-  (state [this]))
-;;
-;;
-
-(do
-  ;;
-  ;; Anything backed by a :native-buffer has a zero
-  ;; copy pathway to and from numpy.
-  ;; Https://clj-python.github.io/libpython-clj/Usage.html
-  (alter-var-root #'hd/default-opts
-                  (fn [m]
-                    (assoc m
-                           :tensor-opts {:container-type
-                                         :native-heap})))
-  (require-python '[numpy :as np])
-  (require-python '[torch :as torch])
-  (require-python '[torch.sparse :as torch.sparse])
-  (require '[libpython-clj2.python.np-array]))
-
-
-(defn brownian-update [])
-
-
-
-
-;; ------------------------
-;;
-;;
-;;
-;;
+  (state [this])
+  (update [this])
+  (append-activations [this activations]))
 
 (def field-size [30 30])
-(torch/randn field-size :device pyutils/*torch-device*)
-
-
-
-
 
 (def particles
   (torch/ge
@@ -75,100 +43,230 @@
 (defn hamming-distance [p1 p2]
   (f/sum (f/not-eq p1 p2)))
 
-(hamming-distance [0 1 0] [0 1 1])
-(hamming-distance [0 1 0] [0 1 1])
-
 (defn l-infnity-dist
   [p1 p2]
   (fm/abs (fm/- (f/reduce-max p1) (f/reduce-max p2))))
 
-(defn local-geometry [p1 p2 dist]
-  (<= (l-infnity-dist p1 p2) dist))
+(defn l1-loss
+  [p1 p2]
+  (f/sum (f/abs (f/- p1 p2))))
 
-(l-infnity-dist [10 0] [0 0])
-(l-infnity-dist [0 0] [10 0])
 
-(local-geometry [0 0] [0 0] 1)
-(local-geometry [0 0] [0 1] 1)
-(local-geometry [0 0] [1 1] 1)
-(local-geometry [0 0] [1 2] 1)
+(comment
+  (l1-loss [0 2] [2 2])
+
+  ;; . . x
+  ;; . . .
+  ;; . . p
+
+  (l1-loss [1 2] [2 2])
+  ;; . . .
+  ;; . . x
+  ;; . . p
+
+  )
+
 
 
 (defn local-geometry-1d [i j]
   (<= 1 (abs (- i j))))
 
-(torch/tril_indices
- (torch/randn [3 3]))
+(defn local-geometry [p1 p2 dist]
+  (< 0 (l1-loss p1 p2) dist))
 
-(dtt/compute-tensor
- [3 3]
- (comp {true 1.0 false 0.0} local-geometry-2d)
- :float32)
+(defn local-square-matrix
+  [grid-width]
+  (let [->point (fn [i] [(fm/mod i grid-width)
+                         (fm/quot i grid-width)])]
+    (dtt/compute-tensor
+      [(* grid-width grid-width) (* grid-width grid-width)]
+      (fn [i j]
+        (if
+            (local-geometry (->point i) (->point j) 2)
+            1.0
+            0.0))
+      :float32)))
 
-(let
-    [grid-size [3 3]]
-  (dtt/compute-tensor
-   grid-size
-   (comp
-    {false 0.0 true 1.0}
-    (fn [i j]
-      [(fm/rem i (grid-size 0))
-       (fm/mod j (grid-size 0))]))
-   :float32))
-
-
-(let [grid-size [3 5]]
-  (dtt/compute-tensor
-   grid-size
-   (fn [i j]
-     [
-      ;; p1:
-      [(fm/rem i (grid-size 0))
-       (fm/mod i (grid-size 0))]
-
-      ;; p2:
-      [(fm/rem j (grid-size 0))
-       (fm/mod j (grid-size 0))]])
-   :object))
-
-
-
-(let [grid-size [3 3]]
-  (dtt/compute-tensor
-   grid-size
-   (fn [i j]
-     [
-      ;; p1:
-      ;; p2:
-      [(fm/rem j (grid-size 0))
-       (fm/mod j (grid-size 0))]
-
-      [(fm/rem i (grid-size 0))
-       (fm/mod i (grid-size 0))]])
-   :object))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(defn field-matrix [field-size]
+(defn field-matrix
+  [field-size]
   ;; i->j, directed graph with geometry around each i
+  (pyutils/ensure-torch
+   (local-square-matrix field-size)))
+
+;; --------------
+
+(defn grid-field
+  [size update-fn]
+  {:activations
+   (torch/zeros [(* size size)] :dtype torch/float)
+   :t 0
+   :size size
+   :update-fn update-fn
+   :weights (field-matrix size)})
+
+;; (defn append-activations-1 [state activations])
+
+(defn update-grid-field
+  [{:as s :keys [activations weights update-fn]}]
+  (-> s
+      (update :t inc)
+      (assoc :activations (update-fn weights activations))))
+
+(defn read-particles [{:keys [activations size]}]
+  (py.. activations (view size size)))
+
+;; -------
+
+(defn brownian-motion
+  [weights]
+  (torch/mul weights
+             (torch/rand_like weights
+                              :device
+                              pyutils/*torch-device*)))
+(defn brownian-update
+  [weights activations]
+  (default-update (brownian-motion weights) activations))
 
 
-  (torch/randn [3 3]))
+
+
+
+;; --------
+
+(defn default-update
+  [weights activations]
+  (let [inputs (torch/mv weights activations)
+        idxs (py.. (torch/topk inputs
+                               (long (py.. (torch/sum
+                                            activations)
+                                       item)))
+               -indices)]
+    (py/set-item! (torch/zeros_like activations) idxs 1)))
+
+
+(def f
+  (grid-field 3 brownian-update))
+
+
+
+
+(comment
+  (default-update
+   (brownian-motion (field-matrix 3))
+   (torch/tensor [1 0 0 0 0 0 0 0 0] :dtype torch/float))
+  (update-grid-field
+   (assoc
+    (update-grid-field f)
+    :activations
+    (torch/tensor [1 0 0 0 0 0 0 0 0] :dtype torch/float)
+    ;; (py.. (torch/ge (torch/rand [(* 3 3)]
+    ;;                             :dtype
+    ;;                             torch/float)
+    ;;                 0.5)
+    ;;   (to :dtype torch/float))
+    ))
+
+  )
+
+
+
+
+
+
+
+
+
+;; ---------
+
+
+
+
+
+
+;;
+;;
+
+(do
+  ;;
+  ;; Anything backed by a :native-buffer has a zero
+  ;; copy pathway to and from numpy.
+  ;; Https://clj-python.github.io/libpython-clj/Usage.html
+  (alter-var-root #'hd/default-opts
+                  (fn [m]
+                    (assoc m
+                           :tensor-opts {:container-type
+                                         :native-heap})))
+  (require-python '[numpy :as np])
+  (require-python '[torch :as torch])
+  (require-python '[torch.sparse :as torch.sparse])
+  (require '[libpython-clj2.python.np-array]))
+
+
+
+
+
+;; ------------------------
+;;
+;;
+;;
+;;
+
+
+
+
+
+
+
+
+
+
+
+
+
+(comment
+  (py..
+      (torch/mv
+       (field-matrix 3)
+       (py..
+           (torch/tensor
+            [[0 1 0]
+             [0 0 0]
+             [0 0 0]]
+            :dtype
+            torch/float)
+           (view -1)))
+      (view 3 3))
+
+  (py..
+      (torch/mv
+       (field-matrix 3)
+       (py..
+           (torch/tensor
+            [[0 0 0]
+             [0 1 0]
+             [0 0 0]]
+            :dtype
+            torch/float)
+         (view 9)))
+    (view 3 3))
+
+
+  (let [inputs
+        (torch/mv (field-matrix 3)
+                  (py.. (torch/tensor [[0 0 0] [0 1 0]
+                                       [0 1 0]]
+                                      :dtype
+                                      torch/float)
+                    (view -1)))
+        idxs (py.. (torch/topk inputs 2) -indices)]
+    (py/set-item! (torch/zeros [9]) idxs 1)))
+
+(comment
+  (let [grid-width 3
+        ->point (fn [i] [(fm/mod i grid-width)
+                         (fm/quot i grid-width)])]
+    (dtt/compute-tensor
+     [(* grid-width grid-width) (* grid-width grid-width)]
+     (fn [i j] [(l1-loss (->point i) (->point j)) :p1
+                (->point i) :p2 (->point j) :i i :j j])
+     :object)))
