f295aa47c4539a4008802c0d439f6016508fdfd2
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2024-09-22 18:43:35 +0200

    Demo

--- src/demo/gravity_sketch.clj
+++ src/demo/gravity_sketch.clj
@@ -14,22 +14,22 @@
 
 (defn setup-objects
   [n]
-  (let [->obj
-        (fn []
-          {:mass 1e10
-           :position [(* 600 (rand)) (* 400 (rand))]
-           :radius 10
-           :color
-           [(* 256 (rand))
-            (* 256 (rand))
-            (* 256 (rand))]
-           :velocity [0 0]})
-        ]
+  (let [->obj (fn []
+                {:color [(* 256 (rand)) (* 256 (rand))
+                         (* 256 (rand))]
+                 :mass 1e10
+                 :position [(* 600 (rand)) (* 400 (rand))]
+                 :radius 10
+                 :velocity [0 0]})]
     (concat [{:mass 1e13
               :position [300 300]
               :radius 5
               :velocity [0 0]}]
             (repeatedly n ->obj))))
+
+
+;; Example "data orientedness" (game development)
+;;
 
 (defn setup
   []
@@ -48,35 +48,33 @@
 
 (defn update-physics
   [{:keys [masses velocities positions]}]
-  (let [forces
-        (g/gravity positions masses)
+  (let [forces (g/gravity positions masses)
         velocities
-        (g/update-velocities velocities forces masses 1)
+          (g/update-velocities velocities forces masses 1)
         positions
-        (g/update-positions positions velocities 1)]
+          (g/update-positions positions velocities 1)]
     {:masses masses
      :positions positions
      :positions-jvm (pyutils/torch->jvm positions)
      :velocities velocities}))
 
-(defn update-state [state]
+(defn update-state
+  [state]
   (merge state (update-physics state)))
 
 (defn draw
   [state]
   (q/background 0)
   (q/fill 255)
-  (time
-   (doall
-    (map (fn [obj [x y]]
-           (q/ellipse x
-                      y
-                      (* 2 (:radius obj))
-                      (* 2 (:radius obj)))
-           ;; (q/with-fill [256 0 0])
-           )
-         (:objects state)
-         (:positions-jvm state)))))
+  (time (doall (map (fn [obj [x y]]
+                      (q/ellipse x
+                                 y
+                                 (* 2 (:radius obj))
+                                 (* 2 (:radius obj)))
+                      ;; (q/with-fill [256 0 0])
+                    )
+                 (:objects state)
+                 (:positions-jvm state)))))
 
 (q/defsketch gravity-sim
   :title "Multi-object Gravity Simulation"
